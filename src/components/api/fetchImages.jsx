f5ae9ffaba8de8c94432002b16359379f14fedcf
IhorMasechko <igor.masechko007@gmail.com>
2023-03-31 13:58:43 +0300

    add new code

---
+++ src/components/api/fetchImages.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = `https://pixabay.com/api`;
+
+export const fetchImages = async (inputValue, pageNr) => {
+  const response = await axios.get(
+    `/?q=${inputValue}&page=${pageNr}&key=33424543-ad7f1dfa8b6f1d99895f0282d&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response.data.hits.map(image => {
+    return {
+      id: image.id,
+      webformatURL: image.webformatURL,
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
+    };
+  });
+};
