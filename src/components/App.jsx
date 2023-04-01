5745cf70fd0bb222634324d7d750c5235bcaf3be
IhorMasechko <igor.masechko007@gmail.com>
2023-04-01 22:23:10 +0300

    updated code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,47 +3,65 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { fetchImages } from './api/fetchImages';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
+import './App.css';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
+import Notiflix from 'notiflix';
 import React from 'react';
+
+let pageNr = 1;
 
 export class App extends Component {
   state = {
     images: [],
-    isLoading: false,
-    currentSearch: '',
-    pageNr: 1,
+    status: 'idle',
+    inputData: '',
     modalOpen: false,
     modalImg: '',
     modalAlt: '',
+    totalHits: 0,
   };
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    const inputForSearch = e.target.elements.inputForSearch;
-    if (inputForSearch.value.trim() === '') {
+  handleSubmit = async inputData => {
+    pageNr = 1;
+    if (inputData.trim() === '') {
+      Notiflix.Notify.info('You cannot search by empty field, try again.');
       return;
+    } else {
+      try {
+        this.setState({ status: 'pending' });
+        const { totalHits, hits } = await fetchImages(inputData, pageNr);
+        if (hits.length < 1) {
+          this.setState({ status: 'idle' });
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          this.setState({
+            images: hits,
+            inputData,
+            totalHits: totalHits,
+            status: 'resolved',
+          });
+        }
+      } catch (error) {
+        this.setState({ status: 'rejected' });
+      }
     }
-    this.setState({ isLoading: true });
-
-    const response = await fetchImages(inputForSearch.value, 1);
-    this.setState({
-      images: response,
-      isLoading: false,
-      currentSearch: inputForSearch.value,
-      pageNr: 1,
-    });
   };
 
   handleClickMore = async () => {
-    const response = await fetchImages(
-      this.state.currentSearch,
-      this.state.pageNr + 1
-    );
-    this.setState({
-      images: [...this.state.images, ...response],
-      pageNr: this.state.pageNr + 1,
-    });
+    this.setState({ status: 'pending' });
+
+    try {
+      const { hits } = await fetchImages(this.state.inputData, (pageNr += 1));
+      this.setState(prevState => ({
+        images: [...prevState.images, ...hits],
+        status: 'resolved',
+      }));
+    } catch (error) {
+      this.setState({ status: 'rejected' });
+    }
   };
 
   handleImageClick = e => {
@@ -73,37 +91,57 @@ export class App extends Component {
   }
 
   render() {
-    return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr',
-          gridGap: '16px',
-          paddingBottom: '24px',
-        }}
-      >
-        {this.state.isLoading ? (
+    const { totalHits, status, images, modalOpen, modalImg, modalAlt } =
+      this.state;
+    if (status === 'idle') {
+      return (
+        <div className="App">
+          <Searchbar onSubmit={this.handleSubmit} />
+        </div>
+      );
+    }
+    if (status === 'pending') {
+      return (
+        <div className="App">
+          <Searchbar onSubmit={this.handleSubmit} />
+          <ImageGallery onImageClick={this.handleImageClick} images={images} />
           <Loader />
-        ) : (
-          <React.Fragment>
-            <Searchbar onSubmit={this.handleSubmit} />
-            <ImageGallery
-              onImageClick={this.handleImageClick}
-              images={this.state.images}
+          {totalHits > 12 && <Button onClick={this.handleClickMore} />}
+          {modalOpen ? (
+            <Modal
+              src={modalImg}
+              alt={modalAlt}
+              handleClose={this.handleModalClose}
             />
-            {this.state.images.length > 11 ? (
-              <Button onClick={this.handleClickMore} />
-            ) : null}
-          </React.Fragment>
-        )}
-        {this.state.modalOpen ? (
-          <Modal
-            src={this.state.modalImg}
-            alt={this.state.modalAlt}
-            handleClose={this.handleModalClose}
-          />
-        ) : null}
-      </div>
-    );
+          ) : null}
+        </div>
+      );
+    }
+    if (status === 'rejected') {
+      return (
+        <div className="App">
+          <Searchbar onSubmit={this.handleSubmit} />
+          <p>Something wrong, try later</p>
+        </div>
+      );
+    }
+    if (status === 'resolved') {
+      return (
+        <div className="App">
+          <Searchbar onSubmit={this.handleSubmit} />
+          <ImageGallery onImageClick={this.handleImageClick} images={images} />
+          {totalHits > 12 && totalHits > images.length && (
+            <Button onClick={this.handleClickMore} />
+          )}
+          {modalOpen ? (
+            <Modal
+              src={modalImg}
+              alt={modalAlt}
+              handleClose={this.handleModalClose}
+            />
+          ) : null}
+        </div>
+      );
+    }
   }
 }
--- src/components/api/fetchImages.jsx
+++ src/components/api/fetchImages.jsx
@@ -2,16 +2,9 @@ import axios from 'axios';
 
 axios.defaults.baseURL = `https://pixabay.com/api`;
 
-export const fetchImages = async (inputValue, pageNr) => {
+export const fetchImages = async (inputData, pageNr) => {
   const response = await axios.get(
-    `/?q=${inputValue}&page=${pageNr}&key=33424543-ad7f1dfa8b6f1d99895f0282d&image_type=photo&orientation=horizontal&per_page=12`
+    `/?q=${inputData}&page=${pageNr}&key=33424543-ad7f1dfa8b6f1d99895f0282d&image_type=photo&orientation=horizontal&per_page=12`
   );
-  return response.data.hits.map(image => {
-    return {
-      id: image.id,
-      webformatURL: image.webformatURL,
-      largeImageURL: image.largeImageURL,
-      tags: image.tags,
-    };
-  });
+  return response.data;
 };
